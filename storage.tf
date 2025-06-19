2b19ea4bce51d59ed39bc6dafc2ec44f95b4fb9c
Mauricio Nasser <mauricio@littlehorse.io>
2025-06-19 10:37:14 -0500

    feat: container for save terraform state (#5)

--- storage.tf
+++ storage.tf
@@ -1,8 +1,15 @@
 resource "azurerm_storage_account" "terraform_state" {
-  name                     = "lhterraformstate${substr(var.subscription_id, 0, 6)}"
-  resource_group_name      = local.resource_group_name
-  location                 = local.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-  depends_on               = [azurerm_resource_group.lh_group]
+  name                            = "lhterraformstate${substr(var.subscription_id, 0, 6)}"
+  resource_group_name             = local.resource_group_name
+  location                        = local.location
+  account_tier                    = "Standard"
+  account_replication_type        = "GRS"
+  allow_nested_items_to_be_public = false
+  depends_on                      = [azurerm_resource_group.lh_group]
+}
+
+resource "azurerm_storage_container" "terraform_state" {
+  name                  = "lh-byoc-terraform-state"
+  storage_account_name  = azurerm_storage_account.terraform_state.name
+  container_access_type = "private"
 }
