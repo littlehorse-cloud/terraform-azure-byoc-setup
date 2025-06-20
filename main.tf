fa1c15820c19bd65bbfcade2a3f2a8e1d63cda54
Mauricio Perez <mauricio@littlehorse.io>
2025-06-20 17:06:15 -0500

    fix: add missing permission

--- main.tf
+++ main.tf
@@ -18,8 +18,14 @@ resource "azuread_service_principal" "github_sa" {
   client_id = azuread_application_registration.github_actions.client_id
 }
 
-resource "azurerm_role_assignment" "github_sa_roll" {
+resource "azurerm_role_assignment" "github_sa_roll_contributor" {
   scope                = data.azurerm_subscription.primary.id
   role_definition_name = "Contributor"
+  principal_id         = azuread_service_principal.github_sa.object_id
+}
+
+resource "azurerm_role_assignment" "github_sa_roll_user_access_admin" {
+  scope                = data.azurerm_subscription.primary.id
+  role_definition_name = "User Access Administrator"
   principal_id         = azuread_service_principal.github_sa.object_id
 }
